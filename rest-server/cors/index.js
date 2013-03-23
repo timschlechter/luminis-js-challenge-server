b93cce05d62e6c01ade28c9c35b82b11a2d8ce34
Dennis Geurts <dennisg@dennisg.nl>
2013-03-23 23:18:44 +0100

    added PUT and DELETE

--- rest-server/cors/index.js
+++ rest-server/cors/index.js
@@ -5,7 +5,7 @@
 var allow = function() {
 	return function(req, res, next) {
 		res.set('Access-Control-Allow-Origin', '*');
-		res.set('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
+		res.set('Access-Control-Allow-Methods', 'GET, PUT, DELETE, POST, OPTIONS');
 		res.set('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
 		next();
 	}
