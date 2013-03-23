2d448fd023d3928131bbaf493e8795349d17b786
Dennis Geurts <dennisg@dennisg.nl>
2013-03-23 21:24:38 +0100

    allow cors, add more  headers

--- rest-server/cors/index.js
+++ rest-server/cors/index.js
@@ -6,7 +6,7 @@ var allow = function() {
 	return function(req, res, next) {
 		res.set('Access-Control-Allow-Origin', '*');
 		res.set('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
-		res.set('Access-Control-Allow-Headers', 'Content-Type');
+		res.set('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
 		next();
 	}
 }
